375f7d3c4ca73d3e3df312ddff130655ba452ed5
Hasini Samarathunga <hasinisama99@gmail.com>
2021-07-22 12:28:27 +0530

    Updated to push function arguments to the stack

--- loader.s
+++ loader.s
@@ -22,7 +22,9 @@
        
     loader:                         ; the loader label (defined as entry point in linker script)
         mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the stack (end of memory area)
-        
+        push dword 3                ; arg3
+        push dword 2                ; arg2
+        push dword 1                ; arg1
         call kmain           	     ; call the function, the result will be in eax
        
     .loop:
