1d85d94b700b1ccdda69ab06c7d0553f4c3dec0a
Hasini Samarathunga <hasinisama99@gmail.com>
2021-07-22 11:24:30 +0530

    set up kernel stack

--- loader.s
+++ loader.s
@@ -4,14 +4,23 @@
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
-
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+    
+    
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
-
+        
+    section .bss
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
+        
+       
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the stack (end of memory area)
+        
     .loop:
         jmp .loop                   ; loop forever
