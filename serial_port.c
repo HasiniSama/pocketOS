7ed0a952cf62adff1f0c6047d7e95ee9d3e619c7
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-06 19:08:01 +0530

    Added all C functions to configure the serial port

---
+++ serial_port.c
@@ -0,0 +1,44 @@
+#include "serial_port.h"
+
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor) {
+  /* Tell the serial port to first expect the highest 8 bits, then the lowest
+   * 8 bits. This is done by sending 0x80 to the line command port
+   */
+  outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
+  outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
+  outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
+}
+
+void serial_configure_line(unsigned short com) {
+  /* Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
+   * Content: | d | b | prty  | s | dl  |
+   * Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
+   * data length of 8 bits, one stop bit, no parity bit, break control
+   * disabled and DLAB disabled
+   */
+  outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+}
+
+void serial_configure_fifo_buffer(unsigned short com) {
+  /* Bit:     | 7 6 | 5  | 4 | 3   | 2   | 1   | 0 |
+   * Content: | lvl | bs | r | dma | clt | clr | e |
+   * Value:   | 1 1 | 0  | 0 | 0   | 1   | 1   | 1 | = 0xC7
+   */
+  outb(SERIAL_FIFO_COMMAND_PORT(com), 0xC7);
+}
+
+void serial_configure_modem(unsigned short com) {
+  /* Bit:     | 7 | 6 | 5  | 4  | 3   | 2   | 1   | 0   |
+   * Content: | r | r | af | lb | ao2 | ao1 | rts | dtr |
+   * Value:   | 0 | 0 | 0  | 0  | 0   | 0   | 1   | 1 | = 0x03
+   */
+  outb(SERIAL_MODEM_COMMAND_PORT(com), 0x03);
+}
+
+
+void serial_configure(unsigned short port, unsigned short baudRate) {
+  serial_configure_baud_rate(port, baudRate);
+  serial_configure_line(port);
+  serial_configure_fifo_buffer(port);
+  serial_configure_modem(port);
+}
