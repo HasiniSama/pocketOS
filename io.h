5d76dd0f76e453ac60fd86622b342be795044784
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-06 19:10:10 +0530

    Added 'in' assembly code instruction

--- io.h
+++ io.h
@@ -8,5 +8,13 @@
      *  @param data The data to send to the I/O port
      */
     void outb(unsigned short port, unsigned char data);
+    
+    /** inb:
+     *  Read a byte from an I/O port.
+     *
+     *  @param  port The address of the I/O port
+     *  @return      The read byte
+     */
+    unsigned char inb(unsigned short port);
 
     #endif /* INCLUDE_IO_H */
