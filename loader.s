600d953eeebdb7db61393e56bc5a80857914fa2d
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-06 17:41:10 +0530

    Removed the extra colon at the end of the text section

--- loader.s
+++ loader.s
@@ -14,7 +14,7 @@
         resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
     
         
-    section .text:                  ; start of the text (code) section
+    section .text                   ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
