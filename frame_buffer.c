b78c23f3d2fb7f80622d5333f6ee3ba9fae6b278
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-06 17:56:18 +0530

    Reorganized the C function with seperate C and header files

---
+++ frame_buffer.c
@@ -0,0 +1,24 @@
+#include "frame_buffer.h"	
+
+static char *fb = (char *)0x000B8000;
+
+void fb_move_cursor(unsigned short pos) {
+	outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+	outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
+	outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+  	outb(FB_DATA_PORT, pos & 0x00FF);
+}
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
+	fb[i] = c;
+	fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+int fb_write(char *buf, unsigned int len){
+    	unsigned int i;
+    	for(i = 0; i<len; i++){
+    		fb_write_cell(i*2, *(buf+i), LIGHT_GREY, BLUE);
+    		fb_move_cursor(2);
+    	}
+    	return i;
+}
--- kmain.c
+++ kmain.c
@@ -1,60 +1,8 @@
-    #include "io.h"
+#include "frame_buffer.c"
 
-    /* The I/O ports */
-    #define FB_COMMAND_PORT         0x3D4
-    #define FB_DATA_PORT            0x3D5
-
-    /* The I/O port commands */
-    #define FB_HIGH_BYTE_COMMAND    14
-    #define FB_LOW_BYTE_COMMAND     15
+void kmain(){
     
-    /* Colors */
-    #define BLUE       1
-    #define LIGHT_GREY 7
-    
-    char *fb = (char *) 0x000B8000;
-    
-    /** fb_write_cell:
-     *  Writes a character with the given foreground and background to position i
-     *  in the framebuffer.
-     *
-     *  @param i  The location in the framebuffer
-     *  @param c  The character
-     *  @param fg The foreground color
-     *  @param bg The background color
-     */
-    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-    {
-        fb[i] = c;
-        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-    }
-    
-    
-    /** fb_move_cursor:
-     *  Moves the cursor of the framebuffer to the given position
-     *
-     *  @param pos The new position of the cursor
-     */
-    void fb_move_cursor(unsigned short pos)
-    {
-        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
-        outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
-        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
-        outb(FB_DATA_PORT,    pos & 0x00FF);
-    }
-    
-    int write(char *buf, unsigned int len){
-    	unsigned int i;
-    	for(i = 0; i<len; i++){
-    		fb_write_cell(i*2, *(buf+i), LIGHT_GREY, BLUE);
-    		fb_move_cursor(2);
-    	}
-    	return i;
-    }
-
-    void kmain(){
-    
-       char buffer[] = "Hello Reader! How are you today?";
-       write(buffer,sizeof(buffer));
+	char buffer[] = "Hello Reader! How are you today?";
+	fb_write(buffer,sizeof(buffer));
        
-    }
+}
