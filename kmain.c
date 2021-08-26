d134235a3d45cc0065dea791cb090f7d2e48d0fc
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-26 14:32:09 +0530

    Added the code to call our program

--- kmain.c
+++ kmain.c
@@ -22,6 +22,21 @@ int kmain(unsigned int ebx){
   	multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
   	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
   	unsigned int address_of_module = modules->mod_start;
+  	
+  	if((mbinfo->mods_count) == 1){
+  		char message[] = "ONE module loaded successfully!";
+  		serial_write(0x3F8,message,sizeof(message));
+  		
+  		typedef void (*call_module_t)(void);
+        	/* ... */
+        	call_module_t start_program = (call_module_t) address_of_module;
+        	start_program();
+        	/* we'll never get here, unless the module code returns */
+
+  	}else{
+  		char message[] = "Error: More than ONE module loaded";
+  		serial_write(0x3F8,message,sizeof(message));
+  	}
   	 
   	return 0;
 }
