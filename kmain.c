164460dfa29f6b7287098739ebf48cee7346554f
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-26 14:25:50 +0530

    Updated to find the program in the memory

--- kmain.c
+++ kmain.c
@@ -1,6 +1,7 @@
 #include "drivers/serial_port.c"
 #include "segmentation/memory_segments.h"
 #include "interrupt/interrupts.h"
+#include "multiboot.h"
 
 /* Function to initialize */
 void init() {
@@ -15,7 +16,12 @@ void init() {
 }
 
 /* Kernel Main */
-void kmain(){
+int kmain(unsigned int ebx){
     	init();
-  	
+    	
+  	multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+  	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+  	unsigned int address_of_module = modules->mod_start;
+  	 
+  	return 0;
 }
