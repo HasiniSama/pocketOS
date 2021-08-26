d43b56b46553256af92395eb77b0c39dcbaf3626
Hasini Samarathunga <hasinisama99@gmail.com>
2021-08-26 14:18:16 +0530

    GRUB instructions on loading the modules added

--- loader.s
+++ loader.s
@@ -1,12 +1,13 @@
     global loader                   ; the entry symbol for ELF
     extern kmain   	             ; the function kmain is defined elsewhere
+    
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
     			
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+    ALIGN_MODULES equ 0x00000001    ; tell GRUB to align modules
+    CHECKSUM     equ -(MAGIC_NUMBER + ALIGN_MODULES )  
+    				     ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
-    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
-    
     
     section .bss
     align 4                         ; align at 4 bytes
@@ -17,11 +18,14 @@
     section .text                   ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
-        dd FLAGS                    ; the flags,
-        dd CHECKSUM                 ; and the checksum
-
-
+        dd ALIGN_MODULES            ; write the align modules instruction
+        dd CHECKSUM                 ; write the checksum
+    
+    mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the stack (end of memory area)
+    
     loader:                         ; the loader label (defined as entry point in linker script)
+	
+    	push ebx                    ; multiboot info in ebx 
     	call kmain                  ; call the function, the result will be in eax
 	
     .loop:
