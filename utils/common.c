80d844201d98a21b56ffa42799b3bc2029dfa946
Hasini Samarathunga <hasinisama99@gmail.com>
2021-09-06 21:00:23 +0530

    Added memset function as utility

---
+++ utils/common.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+// Write len copies of val into dest.
+void memset(u8int *dest, u8int val, u32int len)
+{
+    u8int *temp = (u8int *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
+}
---
+++ utils/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef unsigned int   u32int;
+typedef unsigned char  u8int;
+
+void memset(u8int *dest, u8int val, u32int len);
+
+#endif 
