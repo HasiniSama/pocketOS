fc3d6340810f0e5764e8134c3a534ff60670ba05
Hasini Samarathunga <hasinisama99@gmail.com>
2021-09-27 16:59:38 +0530

    Added User Segments

--- memory/segmentation/memory_segments.c
+++ memory/segmentation/memory_segments.c
@@ -1,12 +1,14 @@
 #include "memory_segments.h"
 
-#define SEGMENT_DESCRIPTOR_COUNT 3
+#define SEGMENT_DESCRIPTOR_COUNT 5
 
 #define SEGMENT_BASE 0
 #define SEGMENT_LIMIT 0xFFFFF
 
-#define SEGMENT_CODE_TYPE 0x9A
-#define SEGMENT_DATA_TYPE 0x92
+#define KERNEL_SEGMENT_CODE_TYPE 0x9A
+#define KERNEL_SEGMENT_DATA_TYPE 0x92
+#define USER_SEGMENT_CODE_TYPE 0xFA
+#define USER_SEGMENT_DATA_TYPE 0xF2
 
 #define SEGMENT_GRANULARITY 0xCF
 
@@ -49,13 +51,15 @@ void gdt_set_gate(s32int index, u32int base_address, u32int limit, u8int access_
 void init_gdt()
 {
 	gdt_ptr_t gdt_ptr;
-	gdt_ptr.size = (sizeof(gdt_entry_t) * 3) - 1;
+	gdt_ptr.size = (sizeof(gdt_entry_t) * 5) - 1;
 	gdt_ptr.address = (u32int)&gdt_entries;
 
   	gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
   	
-  	gdt_set_gate(1, SEGMENT_BASE, SEGMENT_LIMIT, SEGMENT_CODE_TYPE, SEGMENT_GRANULARITY); // Kernel Code segment
-  	gdt_set_gate(2, SEGMENT_BASE, SEGMENT_LIMIT, SEGMENT_DATA_TYPE, SEGMENT_GRANULARITY); // Kernel Data segment
+  	gdt_set_gate(1, SEGMENT_BASE, SEGMENT_LIMIT, KERNEL_SEGMENT_CODE_TYPE, SEGMENT_GRANULARITY); 	// Kernel Code segment
+  	gdt_set_gate(2, SEGMENT_BASE, SEGMENT_LIMIT, KERNEL_SEGMENT_DATA_TYPE, SEGMENT_GRANULARITY); 	// Kernel Data segment
+  	gdt_set_gate(3, SEGMENT_BASE, SEGMENT_LIMIT, USER_SEGMENT_CODE_TYPE, SEGMENT_GRANULARITY); 	// User Code segment
+  	gdt_set_gate(4, SEGMENT_BASE, SEGMENT_LIMIT, USER_SEGMENT_DATA_TYPE, SEGMENT_GRANULARITY); 	// User Data segment
 
   	gdt_flush((u32int)&gdt_ptr);
 	
